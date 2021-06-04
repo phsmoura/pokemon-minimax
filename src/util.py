59f704c150e1edb2fe6eb6f83c2eee7d4666efa0
Pedro Moura <moura.pedro123@gmail.com>
2021-06-04 10:36:27 -0300

    fixed get_damage function

--- src/util.py
+++ src/util.py
@@ -14,8 +14,10 @@ def get_effective_factor(atk_type:str, def_types:list) -> int:
 def get_damage(move_category, move_power, attacker, defender, effective_factor):
     if move_category[:3] == 'phy':
         damage = floor((((2 * attacker.level/5) + 20 * move_power * attacker.attack/50) / defender.defense) + 2) * effective_factor
-    elif move_category[:3] == 'spe':
+    else:
         damage = floor((((2 * attacker.level/5) + 20 * move_power * attacker.spatk/50) / defender.spdef) + 2) * effective_factor
     
     if damage <= 0 and effective_factor != 0:
         damage = 1
+
+    return damage
